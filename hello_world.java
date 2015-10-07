a6982462f8c15261a8bf4316868186b9e2664363
santilobato <kadaroth@gmail.com>
2015-10-07 18:50:36 +0200

    Cambio autor

--- hello_world.java
+++ hello_world.java
@@ -3,6 +3,6 @@ package hello_world;
 public class Hello_world {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		System.out.println("Hello World: To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT");
 		}
 	}
