50d49e130553af929dfeecd034a183e5cb8a1a84
santilobato <kadaroth@gmail.com>
2015-10-07 19:19:24 +0200

    subiendo

--- hello_world.java
+++ hello_world.java
@@ -3,6 +3,6 @@ package hello_world;
 public class Hello_world {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		System.out.println("Hello, World from vilanova");
 		}
 	}
